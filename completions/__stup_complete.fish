d03e244c4af31f51d29a797281f194861f4c7b28
Niam Patel <naz1997@googlemail.com>
2021-01-17 22:26:37 +0000

    Add default stup complete, shift line order

--- completions/__stup_complete.fish
+++ completions/__stup_complete.fish
@@ -1,18 +1,28 @@
+#!/usr/bin/env fish
+
 set -l times week month year previous-week previous-month previous-year
-set -l commands show add edit copy log search add-category list-category set-category order-category rename-category usage version
+set -l commands show add edit copy log search add-category list-category set-category order-category rename-category usage
 set -l paged_commands log usage
 set -l note_commands add
 set -l dated_commands show add  edit
 set -l ranged_commands copy log search
+set -l cat_commands show add edit copy log search set-category rename-category
 set -l name_cat_commands add-category rename-category
 set -l desc_cat_commands add-category set-category
-set -l cat_commands show add edit copy log search set-category rename-category
+
+# read category file path from config
+cat "$HOME/.config/stup.conf" | read -d = -l _ category_dir
 
 # disable file completions
 complete -c stup -f
 
 # subcommands listed in $commands
 complete -c stup -n "not __fish_seen_subcommand_from $commands" -x -a "$commands"
+
+# default `add` behaviour with no subcommands
+complete -c stup -n "not __fish_seen_subcommand_from $commands" -s "@" -l at -d "Note timestamp"
+complete -c stup -n "not __fish_seen_subcommand_from $commands" -s c -l category -a "(cat $category_dir/categories.conf)"
+complete -c stup -n "not __fish_seen_subcommand_from $commands" -s n -l note
 
 # version and help
 complete -c stup -s v -l version -d "Print current version"
@@ -29,14 +39,13 @@ complete -c stup -n "__fish_contains_opt -s @ at ; and __fish_seen_subcommand_fr
 complete -c stup -n "__fish_contains_opt -s f from ; or __fish_contains_opt -s t to ; and __fish_seen_subcommand_from $ranged_commands" -x -a "$times" 
 
 # note options
-complete -c stup -n "__fish_seen_subcommand_from $note_commands" -s n -l note
+complete -c stup -n "__fish_seen_subcommand_from $note_commands" -s n -l note -d "Note content"
 
 # add (no-)pager option for paged commands
 complete -c stup -n "__fish_seen_subcommand_from $paged_commands" -l no-pager -d "Disable pager"
 complete -c stup -n "__fish_seen_subcommand_from $paged_commands" -l pager -d "Use pager"
 
-# store category directory and complete from the contained category file
-cat "$HOME/.config/stup.conf" | read -d = -l _ category_dir
+# complete from category file
 complete -c stup -n "__fish_seen_subcommand_from $cat_commands" -s c -l category -a "(cat $category_dir/categories.conf)"
 
 complete -c stup -n "__fish_seen_subcommand_from $name_cat_commands" -l category-name
